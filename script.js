5b2cc1e3c748d6c96f17de5f23ab087d6050838e
Keith Lee <keithfromseoul@gmail.com>
2023-07-20 00:15:09 -0400

    sending api by html/js commit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors'
 
 const app = express();
 const port = 3000
+
+app.use(cors());
 
 //port&routing practice
 
@@ -9,21 +12,34 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 });
 
-app.get('/dog', (req, res) => {
-  res.send('Doggg')
+app.get('/sound/:name', (req, res) => {
+  // const p = req.params
+  const { name } = req.params
+
+  if (name == 'dog') {
+    res.json({ 'sound':'wofff'})
+  } else if (name == 'cat') {
+    res.json({'sound':'meow'})
+  } else if (name == 'cow') {
+    res.json({ 'sound': 'mooooo' })
+  } else {
+    res.json({'sound':'Unknown'})
+  }
 });
 
-app.get('/cat', (req, res) => {
-  res.send('<h1>Catttt</h1>')
-});
+//simple API complete!! :)))
+
+// app.get('/cat', (req, res) => {
+//   res.send('<h1>Catttt</h1>')
+// });
 
 //request&response variables so we can save time (like every youtubers' url) using 'params'
 
-app.get('/user/:name', (req, res) => {
-  const q = req.params //basically, :id becomes 'params'
-  console.log(q)
-  res.json({'userId':q.name})
-});
+// app.get('/user/:name', (req, res) => {
+//   const q = req.params //basically, :id becomes 'params'
+//   console.log(q)
+//   res.json({'userId':q.name})
+// });
 //:id is like parameter, can be anything in actual url, like /user/keith
 
 //request&response using query method
